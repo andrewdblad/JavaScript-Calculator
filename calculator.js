95e11b182770f0898f3ad6d6dbacb8b10f945d3c
Andrew Blad <114179593+andrewdblad@users.noreply.github.com>
2023-05-06 10:35:49 -0600

    Documentation added

--- calculator.js
+++ calculator.js
@@ -1,22 +1,28 @@
-
+/* Select output box and all buttons */
 const buttons = document.querySelectorAll('button')
 const calculatorDisplay = document.querySelector('.box')
 
+/* Set variables*/
 let calculation = []
 let totalCalc
 
 function calculate(button) {
     const value = button.textContent
     if (value === "CLEAR") {
+        /* Reset calculation array and text output box */
         calculation = []
         calculatorDisplay.textContent = ''
     } else if (value === "=") {
+        /* Performs calculation using built in eval func */
         calculatorDisplay.textContent = eval(totalCalc)
     } else {
+        /* add value to array */
         calculation.push(value)
+        /* join all added values into one string */
         totalCalc = calculation.join('')
+        /* Display string in text box */
         calculatorDisplay.textContent = totalCalc
     }
 }
-
+/* function call on button press */
 buttons.forEach(button => button.addEventListener('click', () => calculate(button)))
